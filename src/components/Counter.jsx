bc060b64f7df55e8e16d3496e840bf83e44a9d99
abhijit181 <abhijit.patil@cctech.co.in>
2024-05-10 20:36:59 +0530

    counter functionality added

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,3 +1,20 @@
-const Counter = () => {};
+import { useState } from "react";
+
+const Counter = () => {
+  const [valueOfCount, setValueOfCount] = useState(0);
+
+  return (
+    <>
+      <h1>Counter App</h1>
+      <p style={{ fontSize: "1.5rem" }}>Count: {valueOfCount}</p>
+      <button onClick={() => setValueOfCount((prevState) => prevState + 1)}>
+        Increment
+      </button>
+      <button onClick={() => setValueOfCount((prevState) => prevState - 1)}>
+        Decrement
+      </button>
+    </>
+  );
+};
 
 export default Counter;
